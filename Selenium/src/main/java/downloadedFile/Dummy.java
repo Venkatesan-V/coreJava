8152456625a84271d184f846d0a8d070bff66a09
Venkatesan-V <venkat.evershine@gmail.com>
2024-06-24 06:43:23 +0530

    Retrieved utube list

--- Selenium/src/main/java/downloadedFile/Dummy.java
+++ Selenium/src/main/java/downloadedFile/Dummy.java
@@ -18,14 +18,14 @@ public class Dummy {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-		driver.get("https://www.youtube.com/@naveenautomationlabs/playlists");
+		driver.get("https://www.youtube.com/playlist?list=PLLS0D9-W-1dnFbSyuXD-u_76fKwbGOEHt");
 		Toolkit.getDefaultToolkit().getSystemClipboard();
 
 		Actions act= new Actions(driver);
 		act.scrollByAmount(0, 2000).perform();
 		Thread.sleep(5000);
 
-		List<WebElement> head = driver.findElements(By.xpath("//h3[@class='style-scope ytd-grid-playlist-renderer']"));
+		List<WebElement> head = driver.findElements(By.xpath("//h3[@class='style-scope ytd-playlist-video-renderer']"));
 		int headCount = head.size();
 		System.out.println(headCount);
 
